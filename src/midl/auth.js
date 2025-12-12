33644775abde67dc6e177d19aef70939972daf89
palaksharma-max <palaksharma24873@gmail.com>
2025-12-12 13:18:25 +0530

    Added auth middleware

---
+++ src/midl/auth.js
@@ -0,0 +1,27 @@
+
+import jwt from "jsonwebtoken";
+export const verifyJWT = (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Please login to continue",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
+};
